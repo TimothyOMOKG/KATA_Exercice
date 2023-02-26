fdc4d17014bf2f7f61b833168c6fa1f3cfd1f490
Timothy <timothyjames@gmail.com>
2023-02-26 21:18:18 +0100

    ajout de tests unitaire

--- src/app/composant/tests/CompteBancaireTest.java
+++ src/app/composant/tests/CompteBancaireTest.java
@@ -4,21 +4,26 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import app.composant.CompteBancaire;
+
 class CompteBancaireTest {
+	
 
 	@Test
 	void testDeposer() {
-		fail("Not yet implemented");
+		CompteBancaire compte = new CompteBancaire("Tim", "James");
+		compte.deposer(2000.50); // on test que le solde a bien ete mis a jour (ajout)
+		org.junit.jupiter.api.Assertions.assertEquals(compte.solde, 2000.5);
 	}
 
 	@Test
 	void testRetrait() {
-		fail("Not yet implemented");
+		CompteBancaire compte2 = new CompteBancaire("Tim", "James");
+		compte2.deposer(2000.50);
+		compte2.retrait(1500.50); // on test que le solde a bien ete mis a jour (ajout)
+		org.junit.jupiter.api.Assertions.assertEquals(compte2.solde, 500.0);
 	}
+	
 
-	@Test
-	void testAffichHisto() {
-		fail("Not yet implemented");
-	}
 
 }
